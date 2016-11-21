6006772a16efb975da664c01eb1d81f6c76aa9a5
wangfeili <wangfeili@raycloud.com>
2016-11-21 11:56:27 +0800

    empty

--- mongodb.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by bealiah on 16-10-4.
- */
-var mongoose = require('mongoose');
-mongoose.Promise = global.Promise;
-var db=mongoose.connect('mongodb://user:pass@localhost:port//xifanya');
-db.connection.on("error", function (error) {  console.log("数据库连接失败：" + error); });
-db.connection.on("open", function () {  console.log("------数据库连接成功！------"); });
-exports.mongoose = mongoose;
